3d543302b533a0f99dd6e01c6f86e6455da0c4ff
RoSSiFuMi1988 <iovine.antonio_1988@libero.it>
2015-07-22 19:41:17 +0200

    modified first page with one Image Switcher.

--- app/src/main/java/store/active/asapp/FirstPageFragment.java
+++ app/src/main/java/store/active/asapp/FirstPageFragment.java
@@ -1,40 +1,78 @@
 package store.active.asapp;
 
-import android.annotation.TargetApi;
 import android.app.Fragment;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import android.widget.RadioGroup;
+import android.widget.ViewSwitcher;
 
 public class FirstPageFragment extends Fragment {
 
 	private String image1_url = "http://www.active-store.it/img/slider01.png";
+	ImageSwitcher imageSwitcher;
+	//array of image resources for the ImageSwitcher
+	int imageResources[] = {
+			R.drawable.slider01,R.drawable.slider02,
+	};
+	Animation slide_in_left, slide_out_right;
+	int curIndex;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-				View rootView = inflater.inflate(R.layout.first_page_fragment, container, false);
+		final View rootView = inflater.inflate(R.layout.first_page_fragment, container, false);
+		//header with logo of Company
 		ImageView imv0 = (ImageView) rootView.findViewById(R.id.imageView0);
-		ImageView imv1 = (ImageView) rootView.findViewById(R.id.imageView1);
-		ImageView imv2 = (ImageView) rootView.findViewById(R.id.imageView2);
 		imv0.setImageResource(R.drawable.logo_completo);
-		imv1.setImageResource(R.drawable.slider01);
-		imv2.setImageResource(R.drawable.slider02);
+
+		//setting behavior of changing image
+		slide_in_left = AnimationUtils.loadAnimation(this.getActivity(), android.R.anim.slide_in_left);
+		slide_out_right = AnimationUtils.loadAnimation(this.getActivity(), android.R.anim.slide_out_right);
+		imageSwitcher = (ImageSwitcher) rootView.findViewById(R.id.imageSwitcher);
+
+		//inizialize the ImageSwitcher
+		imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+			@Override
+			public View makeView() {
+
+				ImageView imageView = new ImageView(getActivity());
+				imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+				FrameLayout.LayoutParams params = new ImageSwitcher.LayoutParams(
+						RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.MATCH_PARENT);
+
+				imageView.setLayoutParams(params);
+				return imageView;
+
+			}
+		});
+
+		//set Index of the image array for first image to show
+		curIndex = 0;
+		imageSwitcher.setImageResource(imageResources[curIndex]);
+		//set Animation for imageSwitcher
+
+		imageSwitcher.setInAnimation(slide_in_left);
+		imageSwitcher.setOutAnimation(slide_out_right);
+
+		//method for automatic switch of the image into the ImageSwitcher
+		imageSwitcher.postDelayed(new Runnable() {
+			int i = 0;
+			public void run() {
+				imageSwitcher.setImageResource(i++ % 2 == 0 ? R.drawable.slider01 : R.drawable.slider02);
+				imageSwitcher.postDelayed(this, 5000);
+			}}, 5000);
+
+
 
 		return rootView;
 	}
