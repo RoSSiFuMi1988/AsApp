165f714e6fa15d8668331519a45f79f8d4036497
RoSSiFuMi1988 <iovine.antonio_1988@libero.it>
2015-07-25 12:35:16 +0200

    modified icon set and header image

--- app/src/main/java/store/active/asapp/NavigationDrawerFragment.java
+++ app/src/main/java/store/active/asapp/NavigationDrawerFragment.java
@@ -30,7 +30,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-import static store.active.asapp.R.drawable.ic_position_activity;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -121,11 +120,11 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
     public List<NavigationItem> getMenu() {
         List<NavigationItem> items = new ArrayList<NavigationItem>();
         //items.add(new NavigationItem("item 1", getResources().getDrawable(R.drawable.ic_menu_check)));
-        items.add(new NavigationItem("Home",getResources().getDrawable(R.drawable.ic_home_variant)));
+        items.add(new NavigationItem("Home",getResources().getDrawable(R.drawable.ic_home)));
         items.add(new NavigationItem("Contatti & Social",getResources().getDrawable(R.drawable.ic_phone)));
-        items.add(new NavigationItem("Inviaci una e-mail",getResources().getDrawable(R.drawable.ic_comment_multiple_outline)));
-        items.add(new NavigationItem("Apertura Ticket Guasto",getResources().getDrawable(R.drawable.ic_comment_multiple_outline)));
-        items.add(new NavigationItem("Dove Siamo",getResources().getDrawable(ic_position_activity)));
+        items.add(new NavigationItem("Inviaci una e-mail",getResources().getDrawable(R.drawable.ic_mail)));
+        items.add(new NavigationItem("Apertura Ticket Guasto",getResources().getDrawable(R.drawable.ic_mail)));
+        items.add(new NavigationItem("Dove Siamo",getResources().getDrawable(R.drawable.ic_location)));
         items.add(new NavigationItem("About Author",getResources().getDrawable(R.drawable.ic_coding)));
         return items;
     }
