3e51b9c0608e0e6abed9f2466a5c15417f70e6d5
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2016-11-22 11:02:23 +0100

    Modify FirebaseIdService

--- app/src/main/java/store/active/asapp/notificationActivity/MyFirebaseInstanceIDService.java
+++ app/src/main/java/store/active/asapp/notificationActivity/MyFirebaseInstanceIDService.java
@@ -1,9 +1,5 @@
 package store.active.asapp.notificationActivity;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
